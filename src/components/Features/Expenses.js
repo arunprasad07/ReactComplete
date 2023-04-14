8e8b1430390d377e89f93ad1d8e19fed415a2bf2
Arun <arunprasad.azhagesan@gmail.com>
2023-04-14 12:49:07 +0000

    Apply styles for add new expense

--- src/components/Features/Expenses.js
+++ src/components/Features/Expenses.js
@@ -1,6 +1,7 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import NewExpense from "../NewExpense/NewExpense";
 
 function Expenses(props) {
   const expenses = [
@@ -26,28 +27,31 @@ function Expenses(props) {
     },
   ];
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-    </Card>
+    <div>
+      <NewExpense />
+      <Card className="expenses">
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        />
+        <ExpenseItem
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        />
+        <ExpenseItem
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        />
+        <ExpenseItem
+          title={expenses[3].title}
+          amount={expenses[3].amount}
+          date={expenses[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
